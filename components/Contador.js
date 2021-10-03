87e5270efda901c3b43ca24946ff2d79ab7d4c82
Jonathan Mojica <jonathanmojicafernandez@gmail.com>
2021-10-03 16:44:10 -0500

    first commit

---
+++ components/Contador.js
@@ -0,0 +1,12 @@
+const Contador = () => {
+    const [contador, setContador] = React.useState(0);
+    const aumentar = () => setContador(contador + 1);
+    const disminuir = () => setContador(contador -1);
+    return (
+        <div CLA>
+            <h2 className={ contador < 0 ? "menor" : "mayor"}>Contador: {contador}</h2><hr />
+            <button onClick={aumentar}>Aumentar</button>
+            <button onClick={disminuir}>Disminuir</button>
+        </div>
+    );
+}
